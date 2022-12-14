f1e58ac684c8573b06d44abcfc9d3feea26d8820
Airiel Altemara <vsokord#gmail.com>
2022-12-14 12:00:39 -0800

    random date generator working

--- src/App.js
+++ src/App.js
@@ -11,6 +11,14 @@ import Description from './Components/description';
 function App() {
   const [date, setDate] = useState("2022-12-13")
 
+  const randomDateGenerator = () => {
+    const start = "1995-06-16T00:00:00"
+    const startDate = new Date(start).getTime()
+    return console.log((new Date(startDate + (Math.random() * (new Date(Date.now()).getTime() - startDate))).toISOString().substring(0, 10)))
+  
+  }
+
+
   useEffect(() => {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=cXF3Tdgf4WckzDo4fITyvAfJ2egdKtCtLOgN37hI&date=${date}`)
     .then(res =>{
@@ -20,7 +28,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Header date={date} randomDateGenerator={randomDateGenerator} />
       <Picture />
       <Description />
     </div>
--- src/Components/header.js
+++ src/Components/header.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react'
 
-export default function Header() {
+
+
+
+
+
+export default function Header(props) {
+    const {randomDateGenerator} = props
     return(
         <div>
-            <button>Random picture</button>
-        header goes here
+            <button onClick={e => randomDateGenerator()} >Random picture</button>
+            <span>Airiel's NASA APOD Generator</span>
             specific date search bar?
+          
     </div>
     )
 }
