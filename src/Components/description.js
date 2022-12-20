8086ef2490123398290ee17898d0b44856ea9b64
Airiel Altemara <vsokord#gmail.com>
2022-12-19 17:25:26 -0800

    picture and description styled components

--- src/Components/description.js
+++ src/Components/description.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+const StyledP = styled.p`
+    align-items: center;
+    margin: 2% 5% 2% 5%;
+`
+
+const StyledA = styled.a`
+    margin: 3%;
+    padding-bottom: 3%;
+    color: darkgrey;
+    background-color: black;
+`
 
 export default function Description(props) {
-    const {date, explanation, copyright, hdurl} = props
+    const { date, explanation, copyright, hdurl } = props
     return (
         <div>
-            <p>Date taken: {date}</p>
-            <p>{explanation}</p>
-            {copyright && <p id='copyright'>Copyright by: {copyright}</p>}
-            <a href={hdurl} target='_blank' >The high resolution version can be found here.</a>
+            <StyledP>Date taken: {date}</StyledP>
+            <StyledP>{explanation}</StyledP>
+            {copyright && <StyledP id='copyright'>Copyright by: {copyright}</StyledP>}
+            <StyledA href={hdurl} target='_blank' >The high resolution version can be found here.</StyledA>
         </div>
     )
 }
--- src/Components/picture.js
+++ src/Components/picture.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+const StyledImg = styled.img`
+    margin: 2%;
+    width: 90vw;
+`
 
 export default function Picture(props) {
     const {url, title} = props
     return(
         <div className='image'>
-            <img src={url} alt={`NASA picture of the Day titled: ${title}`} ></img>
+            <StyledImg src={url} alt={`NASA picture of the Day titled: ${title}`} ></StyledImg>
         </div>
     )
 }
